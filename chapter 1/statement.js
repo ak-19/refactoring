e8b5a348904ac745ec2196ca2d09a4298797cf37
ante <ante.kotarac@gmail.com>
2019-06-02 23:09:40 +0200

    extrac amout for switch section to function

--- chapter 1/statement.js
+++ chapter 1/statement.js
@@ -1,31 +1,11 @@
 function statement(invoice, plays) {
     let volumeCredits = 0;
     let totalAmount = 0;
-    const format = new Intl.NumberFormat(
-        'en-US',
-        {style: 'currency', currency: 'USD', minimumFractionDigits: 2}
-    ).format;
+    const format = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format;
     let result = `Statement for ${invoice.customer}\n`;
     invoice.performances.forEach( performance => {
         const play = plays[performance.playId];
-        let thisAmount = 0;
-        switch (play.type) {
-            case 'Tragedy':
-                thisAmount = 40000;
-                if (performance.audience > 30) {
-                    thisAmount += 1000 * (performance.audience - 30);
-                }
-                break;
-            case 'Comedy':
-                thisAmount = 30000;
-                if (performance.audience > 20) {
-                    thisAmount += 10000 + 500 * (performance.audience - 20);
-                }
-                thisAmount += 300 * performance.audience;
-                break;
-            default:
-                throw new Error('Unknown play type');
-        }
+        let thisAmount = amountForBy(performance, play.type);
         //add volume credits
         volumeCredits += Math.max(performance.audience - 30, 0);
         //add extra credit for every ten comedy atendee
@@ -39,6 +19,28 @@ function statement(invoice, plays) {
 
     result += `Amount owned is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits`;
+    return result;
+}
+
+function amountForBy(performance, playType) {
+    let result = 0;
+    switch (playType) {
+        case 'Tragedy':
+            result = 40000;
+            if (performance.audience > 30) {
+                result += 1000 * (performance.audience - 30);
+            }
+            break;
+        case 'Comedy':
+            result = 30000;
+            if (performance.audience > 20) {
+                result += 10000 + 500 * (performance.audience - 20);
+            }
+            result += 300 * performance.audience;
+            break;
+        default:
+            throw new Error('Unknown play type');
+    }
     return result;
 }
 
