90e17d990cf1480b7a5eb3c91630c0955606e9d9
ante <ante.kotarac@gmail.com>
2021-04-11 15:34:27 +0200

    back to start

--- chapter 1/index.js
+++ chapter 1/index.js
@@ -1,5 +1,5 @@
-const invoices = require("./data-files/invoices");
-const plays = require("./data-files/plays");
-const statement = require("./statement");
+import invoices from "./data-files/invoices.json";
+import plays from "./data-files/plays.json";
+import { statement } from "./code/statement.js";
 
 console.log(statement(invoices[0], plays));
